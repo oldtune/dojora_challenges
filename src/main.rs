c7936df9a7ed304b1eb715a9dc08dae565ced515
do.tran <do.tran@saigontechnology.com>
2022-09-24 16:16:26 +0700

    add logging

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::env;
 
 use actix_cors::Cors;
-use actix_web::{web::Data, App, HttpServer};
+use actix_web::{middleware::Logger, web::Data, App, HttpServer};
 use controllers::challenge_controller::{create_challenge, get_challenge_detail, get_challenges};
 use diesel::{
     r2d2::{ConnectionManager, Pool},
-    PgConnection,
+    Connection, PgConnection,
 };
 use dotenv::dotenv;
 use helpers::db_pool::DbPool;
@@ -23,9 +23,8 @@ mod view_models;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
-
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
     let connection_pool = init_connection_pool(30);
-
     HttpServer::new(move || {
         let cors = Cors::default()
             .allow_any_header()
@@ -33,6 +32,7 @@ async fn main() -> std::io::Result<()> {
             .allow_any_origin();
         let app = App::new()
             .app_data(Data::new(connection_pool.clone()))
+            .wrap(Logger::new("%a %{User-Agent}i"))
             .wrap(cors)
             .service(create_challenge)
             .service(get_challenges)
@@ -47,11 +47,10 @@ async fn main() -> std::io::Result<()> {
 fn init_connection_pool(pool_size: u32) -> DbPool {
     let conn_string = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let conn_manager = ConnectionManager::<PgConnection>::new(&conn_string);
-
     let pool = Pool::builder()
+        .test_on_check_out(true)
         .max_size(pool_size)
         .build(conn_manager)
-        .expect("Failed to init Db pool");
-
+        .unwrap();
     pool
 }
